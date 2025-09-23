e24b9cdee97d50e40f0f2d8dce8800ef6bb368f6
p0kks <218389206+p0kks@users.noreply.github.com>
2025-09-23 13:27:55 +0200

    Update

--- assets/script.js
+++ assets/script.js
@@ -38,3 +38,23 @@ filterButtons.forEach(btn=>{
 
 // Initialize with all projects visible
 filterProjects('all');
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  if (slides.length === 0) return;
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
